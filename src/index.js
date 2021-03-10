d9d65b38584ae9166def1c70dec1ee5fb682665e
Alina Shilo <alinashilo98@gmail.com>
2021-03-10 20:42:42 +0300

    feat: implement function check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [], map = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        map[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === map[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(str[i]);
+        }
+    }
+
+    return stack.length <= 0
+};
